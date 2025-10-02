6b7029bf12297dd4be1074fae37b5a0a5aa8c7ca
ShinY <89335240+ParkShinyeong@users.noreply.github.com>
2025-10-02 09:24:04 +0900

    [level 2] Title: 의상, Time: 0.18 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,18 @@
+import java.util.*; 
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        
+        Map<String, Integer> clothMap = new HashMap<>(); 
+        for(String[] cloth: clothes) {
+            clothMap.put(cloth[1], clothMap.getOrDefault(cloth[1], 0) + 1); 
+        }
+        
+        for(int count: clothMap.values()) {
+            answer *= (count + 1); 
+        }
+        
+        return answer - 1;
+    }
+}
