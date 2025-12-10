81dc46d3badee53627a57694ef5042c8300133a8
ShinY <89335240+ParkShinyeong@users.noreply.github.com>
2025-12-10 23:36:07 +0900

    [Silver II] Title: N과 M (9), Time: 184 ms, Memory: 21948 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -3,7 +3,9 @@ import java.util.*;
 
 public class Main {
     static int n, m; 
-    static TreeSet<String> results; 
+    static boolean[] visited; 
+    static int[] arr, output; 
+    static StringBuilder sb = new StringBuilder(); 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -12,47 +14,43 @@ public class Main {
         n = Integer.parseInt(st.nextToken()); 
         m = Integer.parseInt(st.nextToken()); 
 
-        int[] arr = new int[n]; 
+        arr = new int[n]; 
+        visited = new boolean[n]; 
+        output = new int[m]; 
+
         st = new StringTokenizer(br.readLine()); 
         for(int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken()); 
         }
-        results = new TreeSet<>(
-            (a, b) -> {
-            String[] aarr = a.split(" "); 
-            String[] barr = b.split(" "); 
+        Arrays.sort(arr); 
+        dfs(0); 
 
-            for(int i = 0; i < m; i++) {
-                int aa = Integer.parseInt(aarr[i]); 
-                int bb = Integer.parseInt(barr[i]); 
-                int compareResult = Integer.compare(aa, bb); 
-                if(compareResult == 0)continue; 
-                return compareResult; 
-            }
-            return 0; 
-        }
-    ); 
-
-        dfs(arr, new boolean[n], 0, ""); 
-        for(String str : results) {
-            bw.write(str); 
-            bw.write("\n"); 
-        }
+        bw.write(sb.toString()); 
         bw.flush();
         bw.close();
         br.close();
     }
 
-    static void dfs(int[] arr, boolean[] visited, int depth, String str) {
+    static void dfs(int depth) {
         if(depth >= m) {
-            results.add(str); 
+            for(int num: output) {
+                sb.append(num); 
+                sb.append(" "); 
+            }
+            sb.append("\n"); 
             return;
         }
+        int prev = -1; 
 
         for(int i = 0; i < arr.length; i++) {
-            if(visited[i]) continue; 
+            if(visited[i] || arr[i] == prev) continue; 
+
             visited[i] = true; 
-            dfs(arr, visited, depth + 1, str.length() > 0 ? str + " " + arr[i]: str + arr[i]); 
+            output[depth] = arr[i]; 
+            prev = arr[i]; 
+
+            dfs(depth + 1); 
+
             visited[i] = false; 
         }
     }
