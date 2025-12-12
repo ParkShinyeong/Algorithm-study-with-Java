55b8a2a334497baba60ece9993d294192250143e
ShinY <89335240+ParkShinyeong@users.noreply.github.com>
2025-12-13 00:41:01 +0900

    [Silver II] Title: 키로거, Time: 700 ms, Memory: 128724 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -1,47 +1,40 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int test = Integer.parseInt(br.readLine()); 
-		for(int t = 0; t < test; t++) {
-			String input = br.readLine(); 
-			bw.write(solution(input));
-			bw.write("\n");
-		}
-		bw.flush(); 
-		bw.close();
-		br.close();
-	}
-	public static String solution(String input) {
-		ArrayDeque<Character> prevCursor = new ArrayDeque<>(); 
-		ArrayDeque<Character> nextCursor = new ArrayDeque<>(); 
-		for(char c : input.toCharArray()) {
-			if(c == '-') {
-				if (!prevCursor.isEmpty()) {
-					prevCursor.pollLast(); 
-				}
-			} else if (c == '<') {
-				if(prevCursor.isEmpty()) continue; 
-				char last = prevCursor.pollLast(); 
-				nextCursor.addFirst(last); 
-			} else if (c == '>') {
-				if(nextCursor.isEmpty()) continue; 
-				char first = nextCursor.pollFirst(); 
-				prevCursor.add(first); 
-			} else {
-				prevCursor.add(c); 
-			}
-		}
-		StringBuilder str = new StringBuilder(); 
-		while(!prevCursor.isEmpty()) {
-			str.append(prevCursor.pollFirst()); 
-		}
-		while(!nextCursor.isEmpty()) {
-			str.append(nextCursor.pollFirst()); 
-		}
-		return str.toString(); 
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine()); 
+        for(int t = 0; t < T; t++) {
+            LinkedList<Character> list = new LinkedList<>(); 
+            ListIterator<Character> iter = list.listIterator();
+
+            String input = br.readLine(); 
+            for(char c: input.toCharArray()) {
+                if(c == '<') {
+                    if(iter.hasPrevious()) iter.previous(); 
+
+                } else if(c == '>') {
+                    if(iter.hasNext()) iter.next(); 
+
+                } else if(c == '-') {
+                    if(iter.hasPrevious()) {
+                        iter.previous(); 
+                        iter.remove();
+                    }
+                } else {
+                    iter.add(c); 
+                }
+            }
+            while(iter.hasPrevious()) iter.previous(); 
+    
+            while(iter.hasNext()) {
+                bw.write(iter.next()); 
+            }
+            bw.write("\n"); 
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
