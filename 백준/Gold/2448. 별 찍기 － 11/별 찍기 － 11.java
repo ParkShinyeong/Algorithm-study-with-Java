bfebe7cae047c89d5720f720c579ecf99137847d
ShinY <89335240+ParkShinyeong@users.noreply.github.com>
2026-01-06 19:52:52 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 396 ms, Memory: 121436 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     static char[][] board; 
@@ -8,14 +9,15 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine()); 
         board = new char[N][N * 2 - 1]; 
-        recur(0, N - 1, N); 
-
         for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N * 2 - 1; j++) {
-                bw.write(board[i][j] == '*' ? '*' : ' '); 
-            }
-            bw.write("\n"); 
+            Arrays.fill(board[i], ' '); 
         }
+        recur(0, N - 1, N); 
+        StringBuilder sb = new StringBuilder(); 
+        for(int i = 0; i < N; i++) {
+            sb.append(board[i]).append("\n");  
+        }
+        bw.write(sb.toString()); 
         bw.flush();
         bw.close();
         br.close();
@@ -24,18 +26,15 @@ public class Main {
     static void recur(int sx, int sy, int n) {
         if(n == 3) {
             board[sx][sy] = '*';
-            board[sx + 1][sy - 1] = '*'; 
-            board[sx + 1][sy + 1] = '*'; 
+            board[sx + 1][sy - 1] = board[sx + 1][sy + 1] = '*'; 
             for(int i = sy - 2; i <= sy + 2; i++) {
                 board[sx + 2][i] = '*'; 
             }
             return; 
         }   
-
-        recur(sx, sy, n / 2); 
-        recur(sx + n / 2, sy - n / 2, n / 2); 
-        recur(sx + n / 2, sy + n / 2, n / 2); 
-
-
+        int nxt = n / 2; 
+        recur(sx, sy, nxt); 
+        recur(sx + nxt, sy - nxt, nxt); 
+        recur(sx + nxt, sy + nxt, nxt); 
     }
 }
