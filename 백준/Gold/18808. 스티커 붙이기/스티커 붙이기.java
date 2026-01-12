e1d555d2e8250256b11c446a10d462a0a7d0f07e
ShinY <89335240+ParkShinyeong@users.noreply.github.com>
2026-01-12 16:02:53 +0900

    [Gold III] Title: 스티커 붙이기, Time: 356 ms, Memory: 78592 KB -BaekjoonHub

---
+++ 백준/Gold/18808. 스티커 붙이기/스티커 붙이기.java
@@ -0,0 +1,135 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M, K, answer;
+    static Sticker[] stickers;  
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+        StringTokenizer st = new StringTokenizer(br.readLine()); 
+
+        N = Integer.parseInt(st.nextToken()); 
+        M = Integer.parseInt(st.nextToken()); 
+        K = Integer.parseInt(st.nextToken()); 
+        answer = 0; 
+
+        stickers = new Sticker[K]; 
+
+        for(int s = 0; s < K; s++) {
+            st = new StringTokenizer(br.readLine()); 
+            int n = Integer.parseInt(st.nextToken()); 
+            int m = Integer.parseInt(st.nextToken()); 
+
+            stickers[s] = new Sticker(n, m); 
+
+            for(int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine()); 
+                for(int j = 0; j < m; j++) {
+                    stickers[s].sticker[i][j] = Integer.parseInt(st.nextToken()); 
+                }
+            }
+        }
+
+        dfs(0, new int[N][M]); 
+        bw.write(String.valueOf(answer)); 
+        bw.flush(); 
+        bw.close(); 
+        br.close(); 
+    }
+
+    static void dfs(int idx, int[][] laptop) {
+        if(idx == K) { // base condition
+            int cnt = 0; 
+            for(int i = 0; i < N; i++) {
+                for(int j = 0; j < M; j++) {
+                    if(laptop[i][j] == 1) cnt++; 
+                }
+            }
+            answer = Math.max(answer, cnt); 
+            return; 
+        }
+
+        boolean isSuccess = false; 
+        Sticker tmpSticker = stickers[idx]; 
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                Queue<int[]> puttingPoitions = new ArrayDeque<>(); 
+                isSuccess = putSticker(i, j, laptop, idx, puttingPoitions); 
+
+                if(isSuccess) {
+                    dfs(idx + 1, laptop); 
+                    while(!puttingPoitions.isEmpty()) {
+                        puttingPoitions.poll(); 
+                    }
+                    return; 
+                } else { 
+                    removeSticker(laptop, puttingPoitions);
+                }
+            }
+        }
+
+        if(!isSuccess) {
+            if(tmpSticker.rotateCnt == 4) { 
+                dfs(idx + 1, laptop);
+            } else {
+                Sticker newSticker = rotateSticker90(tmpSticker); 
+                stickers[idx] = newSticker; 
+                dfs(idx, laptop); 
+            }
+         }
+    }
+
+    static boolean putSticker(int sx, int sy, int[][] laptop, int idx, Queue<int[]> puttingQueue) {
+        Sticker tmpSticker = stickers[idx]; 
+
+        for(int i = 0; i < tmpSticker.n; i++) {
+            for(int j = 0; j < tmpSticker.m; j++) {
+                if(tmpSticker.sticker[i][j] == 0) continue; 
+                int nx = sx + i; 
+                int ny = sy + j; 
+                if(nx < 0 || ny < 0 || nx >= N || ny >= M || laptop[nx][ny] == 1) return false;  
+                laptop[nx][ny] = 1; 
+                puttingQueue.offer(new int[]{nx, ny}); 
+            }
+        }  
+        return true; 
+    }
+
+    static void removeSticker(int[][] laptop, Queue<int[]> puttingQueue) { 
+        while(!puttingQueue.isEmpty()) {
+            int[] tmp = puttingQueue.poll(); 
+            laptop[tmp[0]][tmp[1]] = 0;  
+        }
+    }
+
+    static Sticker rotateSticker90(Sticker sticker) {
+        // 스티커를 90도로 돌리기 
+        int[][] tmpSticker = sticker.sticker; 
+        int[][] rotateSticker = new int[sticker.m][sticker.n]; 
+
+        for(int i = 0; i < sticker.n; i++) {
+            for(int j = 0; j < sticker.m; j++) {
+                rotateSticker[j][sticker.n - 1 - i] = tmpSticker[i][j]; 
+            }
+        }
+
+        Sticker newSticker = new Sticker(sticker.m, sticker.n); 
+        newSticker.sticker = rotateSticker; 
+        newSticker.rotateCnt = sticker.rotateCnt + 1; 
+        return newSticker; 
+    }
+}
+
+class Sticker {
+    int n, m; 
+    int[][] sticker; 
+    int rotateCnt; 
+
+    Sticker (int n, int m) {
+        this.n = n; 
+        this.m = m; 
+        sticker = new int[n][m]; 
+        rotateCnt = 0; 
+    }
+}
