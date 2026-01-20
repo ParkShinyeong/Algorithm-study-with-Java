26eb35dcf8cfe4483839f0ea5f2a7ee42968cf91
ShinY <89335240+ParkShinyeong@users.noreply.github.com>
2026-01-20 15:08:01 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 17900 ms, Memory: 114772 KB -BaekjoonHub

--- 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
@@ -1,27 +1,31 @@
 import java.io.*;
-import java.util.*; 
-
+// Counting sort 
 public class Main {
+    static final int total = 2_000_001; 
+    static final int half = 1_000_000; 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
-        
         int N = Integer.parseInt(br.readLine()); 
         int[] arr = new int[N];
-        
+        int[] count = new int[2_000_001]; 
         for(int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine()); 
+            int input = Integer.parseInt(br.readLine()); 
+            count[input + half]++; 
         }
-
-        Arrays.sort(arr); 
-
+        int arrIdx = 0; 
+        for(int i = 0; i < total; i++) {
+            if(arrIdx == N) break; 
+            while(count[i] > 0) {
+                count[i]--; 
+                arr[arrIdx++] = i - half; 
+            }
+        }
         for(int i : arr) {
             bw.write(i + "\n"); 
         }
-
         bw.flush(); 
         bw.close(); 
         br.close(); 
     }
-    
 }
