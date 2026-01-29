b5fe52909a3e0f36d91497cb435adea44035337c
ShinY <89335240+ParkShinyeong@users.noreply.github.com>
2026-01-29 14:41:52 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,27 +1,36 @@
 import java.io.*;
+import java.util.*;
 
-class Main {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
 
-        int n = Integer.parseInt(br.readLine());
-        int[] score = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            score[i] = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine()); 
+        int[] arr = new int[N + 1]; 
+
+        for(int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(br.readLine()); 
         }
 
-        int[] dp = new int[n+1]; 
-        dp[1] = score[1]; 
-        if(n >= 2) dp[2] = score[2] + score[1]; 
-
-        for (int i = 3; i <= n; i++) {
-            dp[i]= Math.max(dp[i-2], dp[i-3] + score[i-1]) + score[i];  
+        if (N == 1) {
+            System.out.println(arr[1]);
+            return;
         }
 
-        bw.write(dp[n] + "");
-        bw.flush();
-        bw.close();
-        br.close();
+        int[][] dp = new int[N + 1][2]; 
+        dp[1][0] = arr[1];
+        dp[2][0] = dp[1][0] + arr[2];
+        dp[2][1] = arr[2];  
+
+        for(int i = 3; i <= N; i++) {
+            dp[i][0] = dp[i - 1][1] + arr[i]; 
+            dp[i][1] = Math.max(dp[i - 2][0], dp[i - 2][1]) + arr[i]; 
+        }
+
+        bw.write(String.valueOf(Math.max(dp[N][0], dp[N][1]))); 
+        bw.flush(); 
+        bw.close(); 
+        br.close(); 
     }
 }
