25483cdc873061a63eb7e1eedc5984d91759a96e
ShinY <89335240+ParkShinyeong@users.noreply.github.com>
2026-01-29 16:10:21 +0900

    [Gold V] Title: 1로 만들기 2, Time: 148 ms, Memory: 22492 KB -BaekjoonHub

--- 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.java
@@ -10,10 +10,10 @@ public class Main {
 
         int[] dp = new int[N + 1]; 
         int[] path = new int[N + 1]; 
-        Arrays.fill(dp, Integer.MAX_VALUE); 
-        dp[0] = dp[1] = 0; 
 
         for(int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1; 
+            path[i] = i - 1; 
             if(i % 3 == 0 && dp[i / 3] + 1 < dp[i]) {
                 dp[i] = dp[i / 3] + 1; 
                 path[i] = i / 3; 
@@ -21,10 +21,6 @@ public class Main {
             if(i % 2 == 0 && dp[i / 2] + 1 < dp[i]) {
                 dp[i] = dp[i / 2] + 1; 
                 path[i] = i / 2; 
-            }
-            if(dp[i - 1] + 1 < dp[i]) {
-                dp[i] = dp[i - 1] + 1; 
-                path[i] = i - 1; 
             }
         }
         bw.write(dp[N] + "\n"); 
