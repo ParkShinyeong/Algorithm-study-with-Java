ae1acff53a64b11de39c7dfb35108d02f8456a90
ShinY <89335240+ParkShinyeong@users.noreply.github.com>
2026-03-19 15:33:36 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 2172 ms, Memory: 419436 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
@@ -3,17 +3,16 @@ import java.util.*;
 
 public class Main {
     static int N; 
-    static boolean[] isChild; 
-    static ArrayList<Integer>[] tree; 
+    static ArrayList<Integer>[] graph; 
+    static int[][] dp; 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
-        tree = new ArrayList[N + 1]; 
-        isChild = new boolean[N + 1]; 
+        graph = new ArrayList[N + 1]; 
         
         for(int i = 1; i <= N; i++) {
-            tree[i] = new ArrayList<>(); 
+            graph[i] = new ArrayList<>(); 
         }
         StringTokenizer st; 
 
@@ -21,38 +20,28 @@ public class Main {
             st = new StringTokenizer(br.readLine()); 
             int p = Integer.parseInt(st.nextToken()); 
             int c = Integer.parseInt(st.nextToken()); 
-            // parents[c] = p; 
-            tree[p].add(c); 
-            tree[c].add(p); 
+            graph[p].add(c); 
+            graph[c].add(p); 
         }
 
-        boolean[] isVisit = new boolean[N + 1]; 
-        int[][] dp = new int[N + 1][2]; 
-        isVisit[1] = true; 
-        dfs(1, dp, isVisit);
-        int root = 0; 
-        for(int i = 1; i <= N; i++) {
-            if(!isChild[i]) root = i; 
-        }
+        dp = new int[N + 1][2]; 
+
+        dfs(1, 0);
         
-        bw.write(Math.min(dp[root][0], dp[root][1]) + ""); 
+        bw.write(Math.min(dp[1][0], dp[1][1]) + ""); 
         bw.flush(); 
         bw.close(); 
         br.close(); 
     }
 
-    private static void dfs(int node, int[][] dp, boolean[] isVisit) {
-        for(int nxt: tree[node]) {
-            if(isVisit[nxt]) continue; 
-            isVisit[nxt] = true; 
-            isChild[nxt] = true; 
-            dfs(nxt, dp, isVisit); 
-        }
+    private static void dfs(int node, int parent) {
+        dp[node][1] = 1; 
+        for(int nxt: graph[node]) {
+            if(nxt == parent) continue; 
+            dfs(nxt, node); 
 
-        for(int nxt: tree[node]) {
             dp[node][0] += dp[nxt][1]; 
             dp[node][1] += Math.min(dp[nxt][0], dp[nxt][1]); 
         }
-        dp[node][1]++; 
     }
 }
