ad7a112c8abbb2fc7244be26b283eceb0dceae6c
ShinY <89335240+ParkShinyeong@users.noreply.github.com>
2026-03-27 15:53:51 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 612 ms, Memory: 64004 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,76 +1,64 @@
 import java.io.*;
 import java.util.*;
 
-class Edge implements Comparable<Edge> {
-	int from;
-	int to ;
-	int weight;
-	public Edge(int from, int to, int weight) {
-		super();
-		this.from = from;
-		this.to = to;
-		this.weight = weight;
-	}
-	@Override
-	public int compareTo(Edge o) {
-		return Integer.compare(this.weight, o.weight); 
-	}
-}
-
 public class Main {
-	static Edge[] edgeList; 
-	static int[] parents; 
-	static int V, E; 
-	static void make() {
-		parents = new int[V + 1]; 
-		for(int i = 1; i <= V; i++) {
-			parents[i] = i; 
-		}
-	}
-	
-	static int find(int a) { 
-		if(a == parents[a]) return a; 
-		return parents[a] = find(parents[a]); 
-	}
-	
-	static boolean union(int a, int b) {
-		int rootA = find(a); 
-		int rootB = find(b); 
-		
-		if(rootA == rootB) return false; 
-		parents[rootA] = rootB; 
-		return true; 
-	}
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine()); 
-		
-		V = Integer.parseInt(st.nextToken()); 
-		E = Integer.parseInt(st.nextToken()); 
-		edgeList = new Edge[E]; 
-		for(int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine()); 
-			int from = Integer.parseInt(st.nextToken()); 
-			int to = Integer.parseInt(st.nextToken()); 
-			int weight = Integer.parseInt(st.nextToken());
-			
-			edgeList[i] = new Edge(from, to, weight); 
-		}
-		
-		make(); 
-		Arrays.sort(edgeList);
-		
-		int result = 0; 
-		for(Edge edge: edgeList) {
-			if(union(edge.from, edge.to)) { 
-				result += edge.weight; 
-			}
-		}
-		bw.write(result+"");
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+    static int V, E; 
+    static ArrayList<int[]>[] graph; 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine()); 
+
+        V = Integer.parseInt(st.nextToken());  
+        E = Integer.parseInt(st.nextToken());  
+        graph = new ArrayList[V + 1]; 
+        for(int i = 0; i <= V; i++) {
+            graph[i] = new ArrayList<>(); 
+        }
+
+        for(int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine()); 
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            graph[v1].add(new int[] {v2, w});
+            graph[v2].add(new int[] {v1, w}); 
+        }
+        bw.write(prim() + ""); 
+        bw.flush();    
+        bw.close(); 
+        br.close(); 
+    }
+
+    private static int prim() {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            return a[2] - b[2]; 
+        }); 
+        
+        
+        int start = 1; 
+        boolean[] visit = new boolean[V + 1]; 
+        visit[start] = true;
+        for(int[] nxt: graph[start]) {
+            pq.offer(new int[] {start, nxt[0], nxt[1]}); // 시작 정점, 끝 정점, 가중치 순
+        }
+        
+        int edgeCnt = 0; 
+        int total = 0; 
+        
+        while(!pq.isEmpty()) {
+            int[] tmp = pq.poll(); 
+            if(edgeCnt == V - 1) break; 
+
+            if(visit[tmp[1]]) continue; 
+            total += tmp[2]; 
+            edgeCnt++; 
+            visit[tmp[1]] = true; 
+            for(int[] nxt: graph[tmp[1]]) {
+                pq.offer(new int[] {tmp[1], nxt[0], nxt[1]});
+            }
+        }
+        return total; 
+    }
 }
