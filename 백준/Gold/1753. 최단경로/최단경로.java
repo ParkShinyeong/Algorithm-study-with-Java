462522c0a5b158b6b73e4dd2723312a82ff404c4
ShinY <89335240+ParkShinyeong@users.noreply.github.com>
2026-04-06 15:42:15 +0900

    [Gold IV] Title: 최단경로, Time: 704 ms, Memory: 110012 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,56 +1,71 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-	static int INF = Integer.MAX_VALUE; 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine()); 
-		int V = Integer.parseInt(st.nextToken());  
-		int E = Integer.parseInt(st.nextToken()); 
-		int K = Integer.parseInt(br.readLine()); 
-		ArrayList<int[]>[] graph = new ArrayList[V + 1]; 
-		for(int i = 0; i <= V; i++) {
-			graph[i] = new ArrayList<int[]>(); 
-		}
-		for(int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine()); 
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			
-			graph[u].add(new int[] { v, w }); 
-		}
-		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
-			return Integer.compare(a[1], b[1]); 
-		}); 
-		int[] distance = new int[V + 1]; 
-		for(int i = 0; i <= V; i++) {
-			distance[i] = INF; 
-		}
-		distance[K] = 0; 
-		pq.offer(new int[] { K, 0 }); 
-		while(!pq.isEmpty()) {
-			int[] current = pq.poll(); 
-			int currentVertex = current[0]; 
-			int currentDistance = current[1]; 
-			if(currentDistance > distance[currentVertex]) continue; 
-			for(int[] edge : graph[currentVertex]) {
-				int nextVertex = edge[0]; 
-				int weight = edge[1]; 
-				
-				if(distance[nextVertex] > currentDistance + weight) { // 현재 값이 원래 값보다 더 작다면
-					distance[nextVertex] = currentDistance + weight; // 값 갱신
-					pq.offer(new int[] {nextVertex, distance[nextVertex]}); // pq에 값 넣어준다. 
-				}
-			}
-		}
-		for(int i = 1; i <= V; i++) {
-			String answer = distance[i] == INF ? "INF\n" : distance[i] + "\n"; 
-			bw.write(answer);
-		}
-		bw.flush(); 
-		bw.close(); 
-		br.close(); 
-	}
+    static int V, E, START;
+    static ArrayList<Node>[] graph; 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine()); 
+        V = Integer.parseInt(st.nextToken()); 
+        E = Integer.parseInt(st.nextToken()); 
+        START = Integer.parseInt(br.readLine()); 
+        graph = new ArrayList[V + 1]; 
+
+        for(int i = 0; i <= V; i++) {
+            graph[i] = new ArrayList<>(); 
+        }
+
+        for(int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine()); 
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            graph[start].add(new Node(end, cost)); 
+        }
+        
+        int[] ans = solution(); 
+        for(int i = 1; i <= V; i++) {
+            if(ans[i] == Integer.MAX_VALUE) bw.write("INF\n"); 
+            else bw.write(ans[i] + "\n"); 
+        }
+
+        bw.flush(); 
+        bw.close(); 
+        br.close(); 
+    }
+
+    private static int[] solution() {
+        int[] length = new int[V + 1]; 
+        Arrays.fill(length, Integer.MAX_VALUE); 
+        length[START] = 0; 
+
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> a.cost - b.cost); 
+        pq.offer(new Node(START, 0)); 
+
+        while(!pq.isEmpty()) {
+            Node tmp = pq.poll(); 
+            if(tmp.cost > length[tmp.value]) continue; 
+            length[tmp.value] = tmp.cost; 
+
+            for(Node nxt: graph[tmp.value]) {
+                int cost = tmp.cost + nxt.cost;
+                if(length[nxt.value] <= cost) continue; 
+                length[nxt.value] = cost;  
+                pq.offer(new Node(nxt.value, cost)); 
+            }
+        }
+        
+        return length; 
+    }
+}
+
+class Node {
+    int value, cost; 
+    Node(int value, int cost) {
+        this.value = value; 
+        this.cost = cost; 
+    }
 }
